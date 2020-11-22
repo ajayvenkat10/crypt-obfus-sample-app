6dbb87d74ddcd6c37342f9b2aabe8feb9d72bfc7
ajaymahadevan <ajayvenkat10@gmail.com>
2020-11-22 16:55:01 +0530

    add api cancellation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:encdec_obfus_sample_app/apis/api.dart';
 import 'package:encdec_obfus_sample_app/models/cc_card_details_model.dart';
@@ -5,6 +6,7 @@ import 'package:encdec_obfus_sample_app/utils/encryption_decryption_utils.dart';
 import 'package:encdec_obfus_sample_app/views/cc_credit_card_item.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_client_helper/http_client_helper.dart' as httpClient;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -14,8 +16,20 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController _nameFieldController = TextEditingController();
   CCCardDetailsModel _creditCard;
+  httpClient.CancellationToken cancellationToken;
 
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _handleGetResponses() {
+    cancellationToken = new httpClient.CancellationToken();
+    for (var i = 0; i < 50; i++) {
+      _handleGetResponse();
+      if (i == 5) {
+        _handleCancel();
+        // print("Hello");
+      }
+    }
+  }
 
   _handleGetResponse() async {
     String question = _nameFieldController.text?.trim();
@@ -30,15 +44,21 @@ class _HomeScreenState extends State<HomeScreen> {
       return;
     }
     try {
-      http.Response response = await http.post(
+      String key = AesCryptHelper.generateAesKey();
+      print(key);
+      final response = await httpClient.HttpClientHelper.post(
         Api.getCreditCard,
-        body: getRequestBody(),
-        headers: getHeaderMap(),
+        body: AesCryptHelper.encryptData(getRequestBody(), key),
+        headers: getHeaderMap(key),
+        cancelToken: cancellationToken,
+        timeRetry: Duration(seconds: 2),
+        retries: 3,
+        timeLimit: Duration(seconds: 30),
       );
 
       if (response.statusCode == 200 && response.body != null) {
         String decryptedResponse =
-            EncryptionDecryptionHelper.decryptData(response.body);
+            AesCryptHelper.decryptData(response.body, key);
         print(decryptedResponse);
         Map<String, dynamic> responseBody = json.decode(decryptedResponse);
         CCCardDetailsModel creditCard =
@@ -55,6 +75,20 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  getRequestBody() {
+    final data = {'name': _nameFieldController.text.toString()};
+    return data.toString();
+  }
+
+  getHeaderMap(String key) {
+    return <String, String>{
+      'Authorization':
+          'Bearer eyJhbGciOiJSU0EtT0FFUC0yNTYiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0.VZKhmwoWIypOPDHK2-hOnS38YVcvO09fMpxnEMzdyWQy0PZgC57LcIRmDOTfgwGuiCMj_GGw7dnMia20DUn_bOfpXvWeOZM5csGn1WWFN-sv3-RFp_F9Ap97hVhJPhdecpUzhkfD67tWFU5BB2kPhMU5hS1CiEcwSL_rEuIPr_5ssvcdhTRUAqMP34j8hMUPnhQodwyrljwD7Y0hvpi1yTdhtTfw1U_sLdc9pUFAq4WdRzlUkMVX-bjEsUtvzAPTYmgtOzd1a163ioxvXPW9Yie2AeSFOeari7xe3IujAjgMNDEtJ_LMyoUc-mYfSsoBiQMpjaR1V6DUNVz7qu4BAA.q8nc-pBLEnfWTymGWBf3IQ._VInoTLuCqBFJaF09AlCMNrF-ttL89sbJEIZibDC_aJR6N34EPGIO90JGqR1wDX9K82XKKG3H8hUL7wQZKgCyTE2xKGgO0ID3RMWmI3liR3GF4fjRFL618csuO52Nqym8tZlHY7mFzBfzBPcSuO2OA.DoqpLAATXvpbQjohzn-VCaUZ-ZH_mcZ_Cx9-UB-q1DE',
+      'IOS_RR1AS9LA97': AesCryptHelper.encryptKey(key),
+      'device_type': 'web'
+    };
+  }
+
   void _handleReset() {
     _nameFieldController.text = " ";
     setState(() {
@@ -62,20 +96,13 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  getRequestBody() {
-    EncryptionDecryptionHelper.generateAESKey(16);
-    final data = {'name': _nameFieldController.text.toString()};
-    return EncryptionDecryptionHelper.encryptData(data.toString());
+  void _handleCancel() {
+    cancellationToken?.cancel();
   }
 
-  getHeaderMap() {
-    return <String, String>{
-      'Authorization':
-          'Bearer eyJhbGciOiJSU0EtT0FFUC0yNTYiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0.VZKhmwoWIypOPDHK2-hOnS38YVcvO09fMpxnEMzdyWQy0PZgC57LcIRmDOTfgwGuiCMj_GGw7dnMia20DUn_bOfpXvWeOZM5csGn1WWFN-sv3-RFp_F9Ap97hVhJPhdecpUzhkfD67tWFU5BB2kPhMU5hS1CiEcwSL_rEuIPr_5ssvcdhTRUAqMP34j8hMUPnhQodwyrljwD7Y0hvpi1yTdhtTfw1U_sLdc9pUFAq4WdRzlUkMVX-bjEsUtvzAPTYmgtOzd1a163ioxvXPW9Yie2AeSFOeari7xe3IujAjgMNDEtJ_LMyoUc-mYfSsoBiQMpjaR1V6DUNVz7qu4BAA.q8nc-pBLEnfWTymGWBf3IQ._VInoTLuCqBFJaF09AlCMNrF-ttL89sbJEIZibDC_aJR6N34EPGIO90JGqR1wDX9K82XKKG3H8hUL7wQZKgCyTE2xKGgO0ID3RMWmI3liR3GF4fjRFL618csuO52Nqym8tZlHY7mFzBfzBPcSuO2OA.DoqpLAATXvpbQjohzn-VCaUZ-ZH_mcZ_Cx9-UB-q1DE',
-      'IOS_RR1AS9LA97': EncryptionDecryptionHelper.encryptKey(),
-      'device_type': 'web'
-    };
-  }
+  // FutureOr _handleTimeout() {
+  //   print('Timed Out');
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -120,11 +147,27 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Text(
                       "Get Credit Card",
                     ),
-                    onPressed: _handleGetResponse,
+                    onPressed: _handleGetResponses,
                     color: Theme.of(context).primaryColor,
                     textColor: Colors.white,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      "Cancel",
+                    ),
+                    onPressed: _handleCancel,
+                    color: Colors.white,
+                    textColor: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      side: BorderSide(
+                          color: Theme.of(context).primaryColor, width: 2),
                     ),
                   ),
                   SizedBox(
--- lib/utils/encryption_decryption_utils.dart
+++ lib/utils/encryption_decryption_utils.dart
@@ -3,60 +3,45 @@ import 'dart:convert';
 import 'package:encrypt/encrypt.dart';
 import 'package:random_string/random_string.dart';
 
-class EncryptionDecryptionHelper {
-  static String _aesKey;
-  static String _aesIv;
-  static Key _key;
-  static IV _iv;
-  static Encrypted _encrypted;
+class AesCryptHelper {
+  static const int _KEY_LENGTH = 16;
 
-  static String get getAESKey {
-    return _aesKey;
-  }
-
-  static String getRSAPublicKey() {
+  static String get _getRSAPublicKey {
     return '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw8MXJisODhbwspUD4XpYTdhCOUXfjdSeGpb306dbFFNqnCovWIQ9uKobfVi6V+WekX6Xw9pXNdEKIwlQceZfv06XUioC1RMQ9X15fyZIUpTyvM2BGE9+8WoAczdDWCLHI099L+5x9oCF4rZL9TBU+sFJap8gNcPktudIjZbwF5Ggh15fq9hp2lhSP5V5bDJpahGV1lyp55W/LmK+Kw58DToNJhl7iCbOmR7AZbPy28Ofq6AW9XdyA93sQKZJkAPoYhvJ3+6nB2FuqwWMDSN/LYTWaxxYFawWr6DlCSWl4k6hG3vKMCXlVemLpfek7r4nTH6XVHzUBjePLLaKJCOfAQIDAQAB\n-----END PUBLIC KEY-----';
   }
 
-  static generateAESKey(int length) {
-    _aesKey = randomAlphaNumeric(length);
-    _key = Key.fromUtf8(_aesKey);
-    _aesIv = randomAlphaNumeric(length);
-    _iv = IV.fromUtf8(_aesIv);
-    print(_aesKey);
+  static generateAesKey() {
+    return randomAlphaNumeric(_KEY_LENGTH);
   }
 
-  static encryptData(String plaintext) {
+  static encryptData(String plaintext, String aesKey) {
     try {
+      final key = Key.fromUtf8(aesKey);
       final encrypter =
-          Encrypter(AES(_key, mode: AESMode.cbc, padding: "PKCS7"));
-      final encrypted = encrypter.encrypt(plaintext, iv: _iv);
-      Uint8List encryptedBytesListWithIv =
-          Uint8List.fromList(_iv.bytes + encrypted.bytes);
-      // print('AES IV: $_aesIv');
-      // print('Encrypted : ${encrypted.base64}');
-      // print('Combined : ${base64.encode(encryptedBytesListWithIv)}');
-      return base64.encode(encryptedBytesListWithIv);
+          Encrypter(AES(key, mode: AESMode.ecb, padding: "PKCS7"));
+      final encrypted = encrypter.encrypt(plaintext);
+      return encrypted.base64;
     } catch (e) {
       throw e;
+      //TODO: Use CC logger
     }
   }
 
-  static encryptKey() {
+  static encryptKey(String aesKey) {
     var keyParser = RSAKeyParser();
     final encrypter = Encrypter(
       RSA(
         publicKey: keyParser.parse(
-          getRSAPublicKey(),
+          _getRSAPublicKey,
         ),
       ),
     );
 
-    final encrypted = encrypter.encrypt(_aesKey);
+    final encrypted = encrypter.encrypt(aesKey);
     return encrypted.base64.toString();
   }
 
-  static decryptData(String encryptedString) {
+  static decryptData(String encryptedString, String aesKey) {
     try {
       // pad the encrypted base64 string with '=' characters until length matches a multiple of 4
       final int toPad = encryptedString.length % 4;
@@ -64,24 +49,18 @@ class EncryptionDecryptionHelper {
         encryptedString =
             encryptedString.padRight(encryptedString.length + toPad, "=");
       }
+      // get cipher bytes
+      final Encrypted encrypted =
+          Encrypted(Uint8List.fromList(base64Decode(encryptedString).toList()));
 
-      // get first 16 bytes which is the initialization vector
-      final iv = IV(Uint8List.fromList(
-          base64Decode(encryptedString).getRange(0, 16).toList()));
-
-      // get cipher bytes (without initialization vector)
-      final Encrypted encrypted = Encrypted(Uint8List.fromList(
-          base64Decode(encryptedString)
-              .getRange(16, base64Decode(encryptedString).length)
-              .toList()));
-
-      // decrypt the string using the key and the initialization vector
-      final key = Key.fromUtf8(_aesKey);
+      // decrypt the string using the key
+      final key = Key.fromUtf8(aesKey);
       final encrypter =
-          Encrypter(AES(key, mode: AESMode.cbc, padding: "PKCS7"));
-      return encrypter.decrypt(encrypted, iv: iv);
+          Encrypter(AES(key, mode: AESMode.ecb, padding: "PKCS7"));
+      return encrypter.decrypt(encrypted);
     } catch (e) {
       throw e;
+      //TODO: Use CC logger
     }
   }
 }
